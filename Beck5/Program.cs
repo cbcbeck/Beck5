07357f69ad128bb2799e050e220f08fbab51b68f
cbcbeck <Hebrews43@gmail.com>
2016-07-25 18:27:31 -0400

    first commit

---
+++ Beck5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beck5
+{
+    class StringOperations
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string for removal of all '0' characters: "); 
+            string phrase = Console.ReadLine(); 
+            StringOperations.FindAndRemoveZeroCharacter(phrase); 
+            Console.ReadKey();
+        }
+        
+        public static void FindAndRemoveZeroCharacter(string text)
+        {
+            string result = string.Empty;
+            string[] words = text.Split('0');  
+            if (words.Length > 1) 
+            {
+                result = string.Join("", words); 
+                Console.WriteLine("The string with all '0' characters removed is: " + result); 
+            }
+            else 
+            {
+                Console.WriteLine("This string is okay!"); 
+            }
+        }
+    }
+}
